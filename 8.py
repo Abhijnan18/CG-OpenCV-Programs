ac94eadd273353c1d8ec12ddd79f0ac8cadebfac
Abhijnan S <114734468+Abhijnan18@users.noreply.github.com>
2024-08-04 21:56:33 +0530

    some small changes

--- 8.py
+++ 8.py
@@ -34,3 +34,31 @@ else:
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+# Read the image
+image = cv2.imread('img1.webp')
+
+if image is not None:
+    h, w = image.shape[:2]
+
+    # Rotation
+    rot_matrix = cv2.getRotationMatrix2D((w/2, h/2), 90, 1)
+    rotated_image = cv2.warpAffine(image, rot_matrix, (w, h))
+
+    # Scaling
+    scaled_image = cv2.resize(image, (0, 0), fx=0.5, fy=0.5)
+
+    # Translation
+    trans_matrix = np.float32([[1, 0, 100], [0, 1, 50]])
+    translated_image = cv2.warpAffine(image, trans_matrix, (w, h))
+
+    cv2.imshow('Original Image', image)
+    cv2.imshow('Rotated Image', rotated_image)
+    cv2.imshow('Scaled Image', scaled_image)
+    cv2.imshow('Translated Image', translated_image)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+else:
+    print("Error: Unable to read the image.")
--- 9.py
+++ 9.py
@@ -7,14 +7,13 @@ if image is None:
     print("Error: Unable to read the image.")
 else:
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
     blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
-
     edges = cv2.Canny(blurred_image, 50, 150)
     cv2.imshow('Edges', edges)
 
     laplacian = cv2.Laplacian(blurred_image, cv2.CV_64F)
-    laplacian = np.uint8(np.absolute(laplacian))
+    laplacian = np.absolute(laplacian)
+    laplacian = np.uint8(laplacian)
     cv2.imshow('Texture (Laplacian)', laplacian)
 
     cv2.waitKey(0)
